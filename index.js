bfe464a5a07a7684d4d1186211aa64ae6f0d1e94
Geoff Dutton <g.dutton@gmail.com>
2019-02-16 08:16:42 -0600

    update packages, require explicity fetch too be passed so node-fetch isn't required in browosers

--- index.js
+++ index.js
@@ -1,60 +1,57 @@
-const retry = require('async-retry');
-const debug = require('debug')('fetch-retry');
+const retry = require('async-retry')
+const debug = console.debug.bind(console, 'fetch-retry')
 
 // retry settings
-const MIN_TIMEOUT = 10;
-const MAX_RETRIES = 3;
-const FACTOR = 5;
+const MIN_TIMEOUT = 10
+const MAX_RETRIES = 3
+const FACTOR = 5
 
-module.exports = setup;
+module.exports = setup
 
-function setup(fetch) {
-  if (!fetch) {
-    fetch = require('node-fetch');
-  }
+function setup (fetch) {
+  fetch = fetch || window.fetch
 
-  function fetchRetry(url, opts = {}) {
+  function fetchRetry (url, opts = {}) {
     const retryOpts = Object.assign({
       // timeouts will be [ 10, 50, 250 ]
       minTimeout: MIN_TIMEOUT,
       retries: MAX_RETRIES,
-      factor: FACTOR,
-    }, opts.retry);
+      factor: FACTOR
+    }, opts.retry)
 
     if (opts.onRetry) {
       retryOpts.onRetry = error => {
-        opts.onRetry(error, opts);
+        opts.onRetry(error, opts)
         if (opts.retry && opts.retry.onRetry) {
-          opts.retry.onRetry(error);
+          opts.retry.onRetry(error)
         }
       }
     }
 
-    return retry(async (bail, attempt) => {
-      const {method = 'GET'} = opts;
-      try {
-        // this will be retried
-        const res = await fetch(url, opts);
-        debug('status %d', res.status);
-        if (res.status >= 500 && res.status < 600) {
-          const err = new Error(res.statusText);
-          err.code = err.status = err.statusCode = res.status;
-          err.url = url;
-          throw err;
-        } else {
-          return res;
-        }
-      } catch (err) {
-        debug(`${method} ${url} error (${err.status}). ${attempt < MAX_RETRIES ? 'retrying' : ''}`, err);
-        throw err;
-      }
+    return retry((bail, attempt) => {
+      const { method = 'GET' } = opts
+      return fetch(url, opts)
+        .then(res => {
+          if (res.status >= 500 && res.status < 600) {
+            const err = new Error(res.statusText)
+            err.code = err.status = err.statusCode = res.status
+            err.url = url
+            throw err
+          } else {
+            return res
+          }
+        })
+        .catch(err => {
+          debug(`${method} ${url} error (${err.status}). ${attempt < MAX_RETRIES ? 'retrying' : ''}`, err)
+          throw err
+        })
     }, retryOpts)
   }
 
   for (const key of Object.keys(fetch)) {
-    fetchRetry[key] = fetch[key];
+    fetchRetry[key] = fetch[key]
   }
-  fetchRetry.default = fetchRetry;
+  fetchRetry.default = fetchRetry
 
-  return fetchRetry;
+  return fetchRetry
 }
--- test.js
+++ test.js
@@ -1,60 +1,62 @@
-const {createServer} =  require('http');
-const retryFetch = require('./index')();
+/* globals test, jest, expect */
+
+const { createServer } = require('http')
+const retryFetch = require('./index')(require('node-fetch'))
 
 test('retries upon 500', async () => {
   let i = 0
   const server = createServer((req, res) => {
     if (i++ < 2) {
-      res.writeHead(500);
-      res.end();
+      res.writeHead(500)
+      res.end()
     } else {
-      res.end('ha');
+      res.end('ha')
     }
-  });
+  })
 
   return new Promise((resolve, reject) => {
     server.listen(async () => {
-      const {port} = server.address();
+      const { port } = server.address()
       try {
-        const res = await retryFetch(`http://127.0.0.1:${port}`);
-        expect(await res.text()).toBe('ha');
-        server.close();
-        resolve();
+        const res = await retryFetch(`http://127.0.0.1:${port}`)
+        expect(await res.text()).toBe('ha')
+        server.close()
+        resolve()
       } catch (err) {
-        reject(err);
+        reject(err)
       }
-    });
-    server.on('error', reject);
-  });
-});
+    })
+    server.on('error', reject)
+  })
+})
 
 test('fails on >MAX_RETRIES', async () => {
   const server = createServer((req, res) => {
-    res.writeHead(500);
-    res.end();
-  });
+    res.writeHead(500)
+    res.end()
+  })
 
   return new Promise((resolve, reject) => {
     server.listen(async () => {
-      const {port} = server.address();
+      const { port } = server.address()
       try {
-        await retryFetch(`http://127.0.0.1:${port}`);
+        await retryFetch(`http://127.0.0.1:${port}`)
       } catch (err) {
-        expect(await err.status).toBe(500);
-        server.close();
-        return resolve();
+        expect(await err.status).toBe(500)
+        server.close()
+        return resolve()
       }
-      reject(new Error('must fail'));
-    });
-    server.on('error', reject);
-  });
-});
+      reject(new Error('must fail'))
+    })
+    server.on('error', reject)
+  })
+})
 
 test('accepts a custom onRetry option', async () => {
   const server = createServer((req, res) => {
-    res.writeHead(500);
-    res.end();
-  });
+    res.writeHead(500)
+    res.end()
+  })
 
   return new Promise((resolve, reject) => {
     const opts = {
@@ -62,19 +64,19 @@ test('accepts a custom onRetry option', async () => {
     }
 
     server.listen(async () => {
-      const {port} = server.address();
+      const { port } = server.address()
       try {
-        await retryFetch(`http://127.0.0.1:${port}`, opts);
+        await retryFetch(`http://127.0.0.1:${port}`, opts)
       } catch (err) {
-        expect(opts.onRetry.mock.calls.length).toBe(3);
-        expect(opts.onRetry.mock.calls[0][0]).toEqual(err);
-        expect(opts.onRetry.mock.calls[0][1]).toEqual(opts);
-        expect(await err.status).toBe(500);
-        server.close();
-        return resolve();
+        expect(opts.onRetry.mock.calls.length).toBe(3)
+        expect(opts.onRetry.mock.calls[0][0]).toEqual(err)
+        expect(opts.onRetry.mock.calls[0][1]).toEqual(opts)
+        expect(await err.status).toBe(500)
+        server.close()
+        return resolve()
       }
-      reject(new Error('must fail'));
-    });
-    server.on('error', reject);
-  });
+      reject(new Error('must fail'))
+    })
+    server.on('error', reject)
+  })
 })
